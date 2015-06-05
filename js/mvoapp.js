b78a1e2c02f33ea9761805ddf7f488b523ac3398
Maggie <jfmaggie@gmail.com>
2015-06-04 23:38:36 -0700

    fix issue: select from list(not showing image yet)

--- js/mvoapp.js
+++ js/mvoapp.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     var Modular = {
+        current: 0,
         catname: ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6'],
-        caturl: ['http://placekitten.com/g/300/400',
-            'http://placekitten.com/g/300/400',
-            'http://placekitten.com/g/300/400',
-            'http://placekitten.com/g/300/400',
-            'http://placekitten.com/g/300/400',
-            'http://placekitten.com/g/300/400'
+        caturl: ['img/cat.jpeg',
+            'img/cat.jpeg',
+            'img/cat.jpeg',
+            'img/cat.jpeg',
+            'img/cat.jpeg',
+            'img/cat.jpeg'
         ],
         catcounts: [],
         init_clickcount: function(numofcat, countarr) {
@@ -24,7 +25,7 @@ $(document).ready(function() {
             // clear old DOM and create cat image
             View.contentElem.html('');
             View.contentElem.append('<div class="catlistholder"></div>');
-            for(var i=0; i<Modular.catname.length; i++){
+            for (var i = 0; i < Modular.catname.length; i++) {
                 // console.log("image here!");
                 $('.catlistholder').append('<div class="item">' + Modular.catname[i] + '<img class="catimage" src="' + Modular.caturl[i] + '">' + '</div>');
             }
@@ -32,7 +33,19 @@ $(document).ready(function() {
 
         }
 
+    };
 
+    var adminView ={
+        init: function(){
+            $('.content').append('<br><div class="admin"><button>Admin</button></div>');
+        },
+        open: function(){
+
+        },
+        close: function(){},
+        update: function(){
+
+        }
     };
 
     var Octopus = (function() {
@@ -45,30 +58,33 @@ $(document).ready(function() {
         View.contentElem.append('<br><div class="selectedcat"></div>');
         View.selectedcat = $('.selectedcat');
 
-        var selectItem = function() {
         $('.catlistholder .item').click(function() {
-            var selected = $(this).clone();
-            var selectedcatname = selected.text();
-            var catindex = Modular.catname.indexOf(selectedcatname);
-            // console.log(catindex);
-            // append the selected cat to the display area and only selected one pic
-            if (View.selectedcat.html() !== '') {
-                $('.catlistholder').append(selected);
-                View.selectedcat.html(selected);
-                // console.log($('.catlistholder'));
-            }
-            View.selectedcat.append(selected);
-            View.selectedcat.append('<div class="item count"></div>');
+                var selected = $(this).clone();
+                var selectedcatname = selected.text();
+                Modular.current = Modular.catname.indexOf(selectedcatname);
+                // console.log(catindex);
+                // append the selected cat to the display area and only selected one pic
+                if (View.selectedcat.html() !== '') {
+                    $('.catlistholder').append(selected);
+                    View.selectedcat.html(selected);
+                    // console.log($('.catlistholder'));
+                }
+                View.selectedcat.append(selected);
+                View.selectedcat.append('<div class="item count"></div>');
+                // console.log(Modular.current);
 
-            // count the click times of selected cat
-            Modular.catcounts[catindex]++;
-            // console.log(this);
-            $('.selectedcat .count').html('You have  clicked  ' + '<span style="color: yellow">' + selected.text() + '</span>' + '  <span style="color: red">  ' + Modular.catcounts[catindex] + ' </span>' + '    times.');
+                // count the click times of selected cat
+            });
+
+        $(View.selectedcat).click(function(){
+            var a =Modular.catcounts[Modular.current]++;
+            $('.count').html(a+1);
+            // console.log(a+1);
         });
-    };
-    selectItem();
 
-    })();
+        adminView.init();
 
+    });
+    Octopus();
 
 });

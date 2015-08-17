a20a0d6bc124e910a56ca036e478113822f513a5
Maggie <jfmaggie@gmail.com>
2015-08-16 17:28:58 -0700

    re-write app

--- js/app.js
+++ js/app.js
@@ -1,79 +1,113 @@
 // Model
-var Cats = [{
-    catName: 'Lily',
-    catUrl: 'http://25.media.tumblr.com/tumblr_maotspCNK41rgiux1o1_1280.jpg',
-    catCount: 0
-}, {
-    catName: 'Lucy',
-    catUrl: 'http://25.media.tumblr.com/tumblr_maotspCNK41rgiux1o1_1280.jpg',
-    catCount: 0
-}, {
-    catName: 'Zach',
-    catUrl: 'http://25.media.tumblr.com/tumblr_maotspCNK41rgiux1o1_1280.jpg',
-    catCount: 0
-}, {
-    catName: 'Mark',
-    catUrl: 'http://25.media.tumblr.com/tumblr_maotspCNK41rgiux1o1_1280.jpg',
-    catCount: 0
-}, {
-    catName: 'Ricky',
-    catUrl: 'http://25.media.tumblr.com/tumblr_maotspCNK41rgiux1o1_1280.jpg',
-    catCount: 0
-}];
-
-var clickCount = function(cat) {
-    var self = cat;
-    self.catCount++;
-    return self.catCount;
+var model = {
+    currentCat: null,
+    cats: [{
+        catName: 'Lily',
+        catUrl: 'img/cat.jpeg',
+        catCount: 0
+    }, {
+        catName: 'Lucy',
+        catUrl: 'img/cat.jpeg',
+        catCount: 0
+    }, {
+        catName: 'Zach',
+        catUrl: 'img/cat.jpeg',
+        catCount: 0
+    }, {
+        catName: 'Mark',
+        catUrl: 'img/cat.jpeg',
+        catCount: 0
+    }, {
+        catName: 'Ricky',
+        catUrl: 'img/cat.jpeg',
+        catCount: 0
+    }]
 };
 
-// View:
-// list all cats by name
-var displayCatList = function(cats) {
-    $('.catholder').append("<ul></ul>");
-    var list = $('.catholder ul');
-    for (var i = 0; i < cats.length; i++) {
-        var currentcat = cats[i];
-        list.append("<li>" + currentcat.catName + "</li>");
-    };
-}(Cats);
+// First View: display current selected cat area
+var catView = {
+    init: function() {
+        this.cat = $('#cat');
+        this.catName = $('#cat-name');
+        this.catImg = $('#cat-img');
+        this.catUrl = $('#cat-url');
+        this.catCount = $('#cat-count');
 
-//display selected cat
-var displaySelectedCat = function(cat) {
-    $('.catholder').prepend("<div class='selectedCat'><h2>" + cat.catName + " " + "<span>" + cat.catCount + "</span>" + "</h2></div>");
-    $('.selectedCat').append("<img src='" + cat.catUrl + "' alt='catspic' />");
-    $('.selectedCat img').click(function() {
-        clickCount(cat);
-        $('.selectedCat h2 span').html(cat.catCount);
-    });
+        this.catImg.on('click', function(){
+            control.incrementCount();
+        });
+
+        // render this view
+        this.render();
+    },
+    render: function() {
+        // update the DOM elem and right values
+        var currentCat = control.getCurrentCat();
+        this.catName.text(currentCat.catName);
+        this.catImg.attr("src",currentCat.catUrl);
+        this.catCount.text(currentCat.catCount);
+
+        // console.log(this.catImg);
+    }
 };
 
-// clear previous selected cat
-var updateCat = function(cat) {
-    $('.selectedCat').detach();
-};
+// Second View: clickable cats list
+var catListView = {
+    init: function(){
+        this.catList = $('#cat-list');
 
-var prevClickedCat;
+        this.render();
+    },
+    render: function() {
+        var cat, elem, i;
+        var cats = control.getCats();
 
-$(document).ready(function() {
-    $('li').click(function(event) {
-        var thisCatName = $(this).text();
-        var thisCat;
+        // empty the cats list
+        this.catList.html('');
 
-        // detach the previous  selected cat
-        if (prevClickedCat) {
-            updateCat(thisCat);
+        // loop over cats array and attach them
+        for(i = 0; i < cats.length; i++){
+            cat  = cats[i];
+            elem = document.createElement('li');
+            elem.textContent = cat.catName;
+
+            elem.addEventListener('click', (function(clickedCat){
+                return function(){
+                    // console.log(clickedCat.catName);
+                    control.setCurrentCat(clickedCat);
+                    catView.render();
+                };
+            })(cat));
+
+            // attach the li elem to catlist
+            this.catList.append(elem);
         }
 
-        // find the selected cat and display it
-        for (var i = 0; i < Cats.length; i++) {
-            if (Cats[i].catName === thisCatName)
-                thisCat = Cats[i];
-        }
-        displaySelectedCat(thisCat);
-        prevClickedCat = thisCat;
+    }
+};
 
+// Contol
+var control = {
+    init: function(){
+        model.currentCat = model.cats[0];
 
-    });
+        catView.init();
+        catListView.init();
+    },
 
-});
+    getCats: function(){
+        return model.cats;
+    },
+    setCurrentCat: function(cat){
+        model.currentCat = cat;
+    },
+    getCurrentCat: function(){
+        return model.currentCat;
+    },
+    incrementCount: function(){
+        model.currentCat.catCount++;
+        catView.render();
+    }
+};
+
+control.init();
